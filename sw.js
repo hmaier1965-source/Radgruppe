5a066dceb7c35ab2dbbcecf0d1cb0daa54a8ed29
hmaier1965-source <hmaier1965@googlemail.com>
2026-05-03 23:32:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'radfreunde-pwa-v1';
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./'])));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
